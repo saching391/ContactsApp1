9b391a9fd7ad49eab5195947a3d93231ba873e78
saching391 <sachin.gokhale@thirdwayv.com>
2019-07-11 10:39:20 -0700

    Initial commit

---
+++ app/src/main/java/com/example/intentschallenge/ContactInfo.java
@@ -0,0 +1,111 @@
+package com.example.intentschallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ContactInfo extends AppCompatActivity {
+
+    EditText etName, etNumber, etWebsite, etAddress;
+    ImageView ivSad, ivNeutral, ivSmiley;
+    TextView tvChoose;
+    Button btnSubmit;
+    static String phoneNumber;
+    static String website;
+    static String address;
+    static String icon;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_info);
+
+        etName = findViewById(R.id.etName);
+        etNumber = findViewById(R.id.etNumber);
+        etWebsite = findViewById(R.id.etWebsite);
+        etAddress = findViewById(R.id.etAddress);
+        ivSad = findViewById(R.id.ivSad);
+        ivNeutral = findViewById(R.id.ivNeutral);
+        ivSmiley = findViewById(R.id.ivSmiley);
+        tvChoose = findViewById(R.id.tvChoose);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+
+
+        ivSad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                icon = "sad";
+            }
+        });
+
+
+        ivNeutral.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                icon = "neutral";
+            }
+        });
+
+
+        ivSmiley.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                icon = "smiley";
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String name = etName.getText().toString().trim();
+                phoneNumber = etNumber.getText().toString().trim();
+                website = etWebsite.getText().toString().trim();
+                address = etAddress.getText().toString().trim();
+
+                Intent intent = new Intent();
+                intent.putExtra("nameData", name);
+                setResult(RESULT_OK, intent);
+
+
+                if (icon.contentEquals("sad"))
+                {
+                    MainActivity.ivIcon.setImageResource(R.drawable.sad);
+                }
+                else if (icon.contentEquals("smiley"))
+                {
+                    MainActivity.ivIcon.setImageResource(R.drawable.smiley);
+                }
+                else
+                {
+                    MainActivity.ivIcon.setImageResource(R.drawable.neutral);
+                }
+
+                if (phoneNumber.contentEquals("") || website.contentEquals("") || address.contentEquals(""))
+                {
+                    Toast.makeText(ContactInfo.this, "Please enter all fields!", Toast.LENGTH_SHORT).show();
+                }
+
+
+                ContactInfo.this.finish();
+
+            }
+        });
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/intentschallenge/MainActivity.java
@@ -0,0 +1,127 @@
+package com.example.intentschallenge;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView tvTitle;
+    Button btnCreateNew;
+    TextView tvName;
+    static ImageView ivIcon;
+    ImageView ivPhone;
+    ImageView ivDirections;
+    ImageView ivWeb;
+
+    final int CONTACTINFO = 69;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCreateNew = findViewById(R.id.btnCreateNew);
+        tvTitle = findViewById(R.id.tvTitle);
+        tvName = findViewById(R.id.tvName);
+        ivPhone = findViewById(R.id.ivPhone);
+        ivDirections = findViewById(R.id.ivDirections);
+        ivWeb = findViewById(R.id.ivWeb);
+        ivIcon = findViewById(R.id.ivIcon);
+
+//        if (ContactInfo.icon.contentEquals("sad"))
+//        {
+//            ivIcon = findViewById(R.id.ivSad);
+//        }
+//        else if (ContactInfo.icon.contentEquals("smiley"))
+//        {
+//            ivIcon = findViewById(R.id.ivSmiley);
+//        }
+//        else
+//        {
+//            ivIcon = findViewById(R.id.ivNeutral);
+//        }
+
+
+
+
+        tvName.setVisibility(View.GONE);
+        ivIcon.setVisibility(View.GONE);
+        ivPhone.setVisibility(View.GONE);
+        ivDirections.setVisibility(View.GONE);
+        ivWeb.setVisibility(View.GONE);
+
+        btnCreateNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, com.example.intentschallenge.ContactInfo.class);
+                startActivityForResult(intent, CONTACTINFO);
+
+                tvName.setVisibility(View.VISIBLE);
+                ivIcon.setVisibility(View.VISIBLE);
+                ivPhone.setVisibility(View.VISIBLE);
+                ivDirections.setVisibility(View.VISIBLE);
+                ivWeb.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        ivPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + ContactInfo.phoneNumber));
+                startActivity(intent1);
+            }
+        });
+
+
+        ivDirections.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=" + ContactInfo.address));
+                startActivity(intent2);
+            }
+        });
+
+
+        ivWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3 = new Intent(Intent.ACTION_VIEW, Uri.parse(ContactInfo.website));
+                startActivity(intent3);
+            }
+        });
+
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CONTACTINFO)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                tvName.setText(data.getStringExtra("nameData"));
+
+            }
+            if (resultCode == RESULT_CANCELED)
+            {
+                tvName.setText("Unknown");
+            }
+        }
+    }
+
+}
